b1d69bb0a102bbd2595fb8e082e4af26028de990
Vladimir <prosto24061989>
2017-11-02 10:29:02 +0200

    1 comit

---
+++ shop.sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 30 2017 г., 09:19
+-- Версия сервера: 5.6.37
+-- Версия PHP: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `parent_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `keywords` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `category`
+--
+
+INSERT INTO `category` (`id`, `parent_id`, `name`, `keywords`, `description`) VALUES
+(31, 0, 'Шапки женские', '', ''),
+(32, 0, 'Шапки мужские', '', ''),
+(33, 0, 'Сумки', '', ''),
+(34, 31, 'подкатегория женских шапок 1', '', ''),
+(35, 31, 'подкатегория женских шапок 2', '', ''),
+(36, 31, 'подкатегория женских шапок 3', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `image`
+--
+
+CREATE TABLE `image` (
+  `id` int(11) NOT NULL,
+  `filePath` varchar(400) NOT NULL,
+  `itemId` int(20) NOT NULL,
+  `isMain` int(1) DEFAULT NULL,
+  `modelName` varchar(150) NOT NULL,
+  `urlAlias` varchar(400) NOT NULL,
+  `name` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `image`
+--
+
+INSERT INTO `image` (`id`, `filePath`, `itemId`, `isMain`, `modelName`, `urlAlias`, `name`) VALUES
+(1, 'Products/Product18/cfd007.jpg', 18, 1, 'Product', '54f68beb07-1', ''),
+(2, 'Products/Product15/20f9b6.jpg', 15, 1, 'Product', '79f44e0638-1', ''),
+(3, 'Products/Product16/9264cc.jpg', 16, 1, 'Product', '8a398d7a0e-1', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `qty` int(10) NOT NULL,
+  `sum` float NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `order_id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `qty_item` int(11) NOT NULL,
+  `sum_item` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `content` text,
+  `price` float NOT NULL DEFAULT '0',
+  `keywords` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `img` varchar(255) DEFAULT 'no-image.png',
+  `hit` enum('0','1') NOT NULL DEFAULT '0',
+  `new` enum('0','1') NOT NULL DEFAULT '0',
+  `sale` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `product`
+--
+
+INSERT INTO `product` (`id`, `category_id`, `name`, `content`, `price`, `keywords`, `description`, `img`, `hit`, `new`, `sale`) VALUES
+(15, 34, 'Шапка', '<p>Описание</p>\r\n', 100, '', '', 'no-image.png', '1', '1', '0'),
+(16, 34, 'Шапка', '<p>Описание</p>\r\n', 0, '', '', 'no-image.png', '1', '1', '0'),
+(17, 34, 'Шапка', 'Описание', 0, NULL, NULL, 'no-image.png', '1', '1', '0'),
+(18, 31, 'Наименование', '<p><strong>КонтентКонтентКонтентКонтент</strong></p>\r\n', 150, '', '', 'no-image.png', '1', '1', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `auth_key` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `auth_key`) VALUES
+(1, 'admin', '$2y$13$/NXRWwQXRmxfMQuWFceTM./AndWPseCMjNEt/eoYHCYTLLV9dwkly', 'L4etUEC430t-KTXTULu-6yJBpwphfFFh');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+--
+-- AUTO_INCREMENT для таблицы `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ views/site/main.php
@@ -0,0 +1,112 @@
+<section id="slider"><!--slider-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div id="slider-carousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#slider-carousel" data-slide-to="1"></li>
+                        <li data-target="#slider-carousel" data-slide-to="2"></li>
+                    </ol>
+
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <div class="col-sm-6">
+                                <h1><span>Ш</span>апки-Сумки</h1>
+                                <h2>Новинки осени</h2>
+                                <p>Большой выбор выбор. </p>
+                                <button type="button" class="btn btn-default get">Посмотреть сейчас</button>
+                            </div>
+                            <div class="col-sm-6">
+                                <img src="/images/home/girl1.jpg" class="girl img-responsive" alt="" />
+<!--                                <img src="/images/home/pricing.png"  class="pricing" alt="" />-->
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-sm-6">
+                                <h1><span>З</span>Заголовок</h1>
+                                <h2>текст</h2>
+                                <p>Текст</p>
+                                <button type="button" class="btn btn-default get">Посмотреть сейчас</button>
+                            </div>
+                            <div class="col-sm-6">
+                                <img src="/images/home/girl2.jpg" class="girl img-responsive" alt="" />
+<!--                                <img src="/images/home/pricing.png"  class="pricing" alt="" />-->
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="col-sm-6">
+                                <h1><span>З</span>Заголовок</h1>
+                                <h2>текст</h2>
+                                <p>Текст</p>
+                                <button type="button" class="btn btn-default get">Посмотреть сейчас</button>
+                            </div>
+                            <div class="col-sm-6">
+                                <img src="/images/home/girl3.jpg" class="girl img-responsive" alt="" />
+<!--                                <img src="/images/home/pricing.png" class="pricing" alt="" />-->
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+                        <i class="fa fa-angle-left"></i>
+                    </a>
+                    <a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section><!--/slider-->
+
+<section><!--content-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="bl bl__item">
+                    <div class="text">
+                        <h1>О компании</h1>
+                        <p>Модные тенденции – понятие переменчивое, как и вкусы покупателей. Именно на этом стремлении к новизне, оригинальности и одновременно комфорту основывается деятельность процветающего производителя головных уборов и сумок. Постоянный приток покупателей –  главное доказательство коммерческого успеха компании. </p>
+                        <p>Интернет-магазин "Shapki-Sumki". Оптовая и розничная продажа головных уборов и сумок. Стильные качественные шапки и сумки для детей и взрослых. Только Новинки 2017.  Сотрудничаем с крупными производителями головных уборов ― Shella ,Sample, Alex, Ozzi, Berxon. Наша продукция отличается высоким качеством! Доставка по всей России.</p>
+                    </div>
+                    <div class="image"><img src="https://shlyapka.com/images/category-women.jpg.pagespeed.ce.YeAN6n7qcK.jpg" alt=""></div>
+                </div>
+                <div class="bl bl__item">
+                    <div class="image"><img src="https://krasota-ua.com/uploads/images/bimages/1507115383.jpg" alt=""></div>
+                    <div class="text">
+                        <h1>Ассортимент</h1>
+                        <p>Что представлено в ассортименте компании? Здесь вы сможете купить шапки для мужчин, женщин и детей. Просмотрев каталог, вы заметите, что здесь можно заказать головные уборы самых разных стилей и имеющих различное предназначение.
+
+                            Например, здесь можно купить вязаные шапки для людей, которые в силу тех или иных причин много времени проводят на улице. Эффективная защита от морозов – главная задача таких головных уборов. Логично, что главный критерий в данном случае – это сохранение тепла, в чём помогают качественные материалы и плотная вязка.
+
+                            Кроме того, в ассортименте представлены модные женские и мужские шапки от производителя. Стильный дизайн, яркие цвета, интересные детали и, конечно, соответствие модным тенденциям – всё это важно для каждого человека, не равнодушного к собственному внешнему виду и современным модным трендам. Конечно же, представленные товары отличаются не только дизайном, но и высоким качеством и отличным выполнением своей непосредственной функции (защита от холодов и осадков).
+                        </p>
+                        <p>Например, у нас вы сможете купить шапки таких видов:
+                            <ul>
+                            <li>Чулок</li>
+                            <li>Сбавка</li>
+                            <li>Немка</li>
+                            <li>Заклёпка</li>
+                            </ul>
+                            Кроме того, в каталоге представлены модели типов «патриотка», «рожки», ушанка. Стильные детали в виде вышивки, помпона или подворота имеют все шансы стать «изюминкой» представленного товара. Так что у нас можно подобрать по-настоящему стильный вариант – благо, выбор широк!
+                        </p>
+                    </div>
+
+                </div>
+                <div class="bl bl__item">
+                    <div class="text">
+                        <h1>Розничная сеть</h1>
+                        <p>Наш ассортимент вы можите найти во многих городах России. Мы имеем большое колличество магазинов с прекрасным персоналом, который поможет Вам определиться с выбором.</p>
+                    </div>
+                    <div class="image"><img src="https://shlyapka.com/images/category-men.jpg.pagespeed.ce.9Oi3HA7t9m.jpg" alt=""></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
